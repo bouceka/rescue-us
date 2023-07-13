5cc01864146572e1a8da3d3e7ddc4c11093e5092
Adam Boucek <adam@boucek.dev>
2023-07-13 12:09:04 -0700

    Feat: Dev preparation

--- Services/SearchService/Program.cs
+++ Services/SearchService/Program.cs
@@ -5,16 +5,11 @@
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+
 
 app.UseHttpsRedirection();
 
