3cf74c3e57171eb30cae49074107a754f9231bd5
Adam Boucek <adam@boucek.dev>
2023-07-13 14:32:42 -0700

    Feat: Connect Animal Service to the SQLserver

--- Services/AnimalService/Program.cs
+++ Services/AnimalService/Program.cs
@@ -1,13 +1,19 @@
-﻿var builder = WebApplication.CreateBuilder(args);
+﻿using AnimalService.Data;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddDbContext<AnimalDbContext>(option =>
+{
+    option.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
 
 var app = builder.Build();
-
 
 
 app.UseHttpsRedirection();
