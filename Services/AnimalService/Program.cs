17bc651426e7fb43abee00d133b89f2f178e1021
Adam Boucek <adam@boucek.dev>
2023-07-20 10:26:37 -0700

    Feat: Connect to RabbitMq with mass transit in Animal Service

--- Services/AnimalService/Program.cs
+++ Services/AnimalService/Program.cs
@@ -1,4 +1,6 @@
-﻿using AnimalService.Data;
+﻿using AnimalService.Consumers;
+using AnimalService.Data;
+using MassTransit;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -15,6 +17,31 @@ builder.Services.AddDbContext<AnimalDbContext>(option =>
 
 builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
 
+// Configure RabbitMq
+builder.Services.AddMassTransit(x =>
+{
+    // add in memory outbox
+    x.AddEntityFrameworkOutbox<AnimalDbContext>(o =>
+    {
+        o.QueryDelay = TimeSpan.FromSeconds(10);
+
+        o.UseSqlServer();
+        o.UseBusOutbox();
+    });
+
+    x.SetEndpointNameFormatter(new KebabCaseEndpointNameFormatter("animal", false));
+
+    x.UsingRabbitMq((context, cfg) =>
+    {
+
+        cfg.Host(builder.Configuration["RabbitMq:Host"], "/", host =>
+        {
+            host.Username(builder.Configuration.GetValue("RabbitMq:Username", "guest"));
+            host.Password(builder.Configuration.GetValue("RabbitMq:Password", "guest"));
+        });
+        cfg.ConfigureEndpoints(context);
+    });
+});
 
 var app = builder.Build();
 
