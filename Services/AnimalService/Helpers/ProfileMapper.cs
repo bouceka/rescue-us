08047430eae015e47346b49d66bcc7443e85a5dc
Adam Boucek <adam@boucek.dev>
2023-07-20 10:31:15 -0700

    Feat: Add event classes to the Animal Service mapper

--- Services/AnimalService/Helpers/ProfileMapper.cs
+++ Services/AnimalService/Helpers/ProfileMapper.cs
@@ -5,14 +5,17 @@ using System.Threading.Tasks;
 using AnimalService.DTOs;
 using AnimalService.Entities;
 using AutoMapper;
+using Events;
 
 namespace AnimalService.Helpers
 {
     public class ProfileMapper:Profile
     {
         public ProfileMapper(){
-             CreateMap<CreateAnimalDto, Animal>();
              CreateMap<Animal, AnimalDto>();
+             CreateMap<CreateAnimalDto, Animal>();
+             CreateMap<AnimalDto, AnimalCreated>();
+             CreateMap<Animal, AnimalUpdated>();
         }
     }
 }
