19f757eade5cf2528141bb4cb5f96b38017fae77
Adam Boucek <adam@boucek.dev>
2023-07-20 10:29:21 -0700

    Feat: Update database with outbox

---
+++ Services/AnimalService/Data/Migrations/20230719201126_Outbox.cs
@@ -0,0 +1,172 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace AnimalService.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class Outbox : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Animals",
+                keyColumn: "Id",
+                keyValue: new Guid("4b22475e-dc6d-40be-866f-32f6aae242a9"));
+
+            migrationBuilder.DeleteData(
+                table: "Animals",
+                keyColumn: "Id",
+                keyValue: new Guid("62aa9d4a-40ea-46ab-9186-789618d5e014"));
+
+            migrationBuilder.CreateTable(
+                name: "InboxState",
+                columns: table => new
+                {
+                    Id = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    MessageId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    ConsumerId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    LockId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    RowVersion = table.Column<byte[]>(type: "rowversion", rowVersion: true, nullable: true),
+                    Received = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    ReceiveCount = table.Column<int>(type: "int", nullable: false),
+                    ExpirationTime = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    Consumed = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    Delivered = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    LastSequenceNumber = table.Column<long>(type: "bigint", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_InboxState", x => x.Id);
+                    table.UniqueConstraint("AK_InboxState_MessageId_ConsumerId", x => new { x.MessageId, x.ConsumerId });
+                });
+
+            migrationBuilder.CreateTable(
+                name: "OutboxMessage",
+                columns: table => new
+                {
+                    SequenceNumber = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    EnqueueTime = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    SentTime = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Headers = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Properties = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    InboxMessageId = table.Column<Guid>(type: "uniqueidentifier", nullable: true),
+                    InboxConsumerId = table.Column<Guid>(type: "uniqueidentifier", nullable: true),
+                    OutboxId = table.Column<Guid>(type: "uniqueidentifier", nullable: true),
+                    MessageId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    ContentType = table.Column<string>(type: "nvarchar(256)", maxLength: 256, nullable: false),
+                    Body = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    ConversationId = table.Column<Guid>(type: "uniqueidentifier", nullable: true),
+                    CorrelationId = table.Column<Guid>(type: "uniqueidentifier", nullable: true),
+                    InitiatorId = table.Column<Guid>(type: "uniqueidentifier", nullable: true),
+                    RequestId = table.Column<Guid>(type: "uniqueidentifier", nullable: true),
+                    SourceAddress = table.Column<string>(type: "nvarchar(256)", maxLength: 256, nullable: true),
+                    DestinationAddress = table.Column<string>(type: "nvarchar(256)", maxLength: 256, nullable: true),
+                    ResponseAddress = table.Column<string>(type: "nvarchar(256)", maxLength: 256, nullable: true),
+                    FaultAddress = table.Column<string>(type: "nvarchar(256)", maxLength: 256, nullable: true),
+                    ExpirationTime = table.Column<DateTime>(type: "datetime2", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_OutboxMessage", x => x.SequenceNumber);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "OutboxState",
+                columns: table => new
+                {
+                    OutboxId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    LockId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    RowVersion = table.Column<byte[]>(type: "rowversion", rowVersion: true, nullable: true),
+                    Created = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Delivered = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    LastSequenceNumber = table.Column<long>(type: "bigint", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_OutboxState", x => x.OutboxId);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Animals",
+                columns: new[] { "Id", "Age", "Breed", "Color", "CoverImageUrl", "CreatedAt", "Description", "Name", "PublicId", "Sex", "Status", "Type", "UpdatedAt", "Weight" },
+                values: new object[,]
+                {
+                    { new Guid("c63e530e-33ee-4970-8e87-92df1274a456"), 5, "Bengal cat", "Beige", "https://placekitten.com/200/200", new DateTime(2023, 7, 19, 20, 11, 26, 94, DateTimeKind.Utc).AddTicks(1500), "lorem ipsum", "Buttercup", 2, "Male", 0, "Cat", new DateTime(2023, 7, 19, 20, 11, 26, 94, DateTimeKind.Utc).AddTicks(1500), 5 },
+                    { new Guid("dea7afd6-c58d-4ac5-985e-66211073cd00"), 2, "Double doodle", "White", "https://placedog.net/500", new DateTime(2023, 7, 19, 20, 11, 26, 94, DateTimeKind.Utc).AddTicks(1480), "lorem ipsum", "Dee Dee", 1, "Female", 0, "Dog", new DateTime(2023, 7, 19, 20, 11, 26, 94, DateTimeKind.Utc).AddTicks(1480), 10 }
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_InboxState_Delivered",
+                table: "InboxState",
+                column: "Delivered");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_OutboxMessage_EnqueueTime",
+                table: "OutboxMessage",
+                column: "EnqueueTime");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_OutboxMessage_ExpirationTime",
+                table: "OutboxMessage",
+                column: "ExpirationTime");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_OutboxMessage_InboxMessageId_InboxConsumerId_SequenceNumber",
+                table: "OutboxMessage",
+                columns: new[] { "InboxMessageId", "InboxConsumerId", "SequenceNumber" },
+                unique: true,
+                filter: "[InboxMessageId] IS NOT NULL AND [InboxConsumerId] IS NOT NULL");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_OutboxMessage_OutboxId_SequenceNumber",
+                table: "OutboxMessage",
+                columns: new[] { "OutboxId", "SequenceNumber" },
+                unique: true,
+                filter: "[OutboxId] IS NOT NULL");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_OutboxState_Created",
+                table: "OutboxState",
+                column: "Created");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "InboxState");
+
+            migrationBuilder.DropTable(
+                name: "OutboxMessage");
+
+            migrationBuilder.DropTable(
+                name: "OutboxState");
+
+            migrationBuilder.DeleteData(
+                table: "Animals",
+                keyColumn: "Id",
+                keyValue: new Guid("c63e530e-33ee-4970-8e87-92df1274a456"));
+
+            migrationBuilder.DeleteData(
+                table: "Animals",
+                keyColumn: "Id",
+                keyValue: new Guid("dea7afd6-c58d-4ac5-985e-66211073cd00"));
+
+            migrationBuilder.InsertData(
+                table: "Animals",
+                columns: new[] { "Id", "Age", "Breed", "Color", "CoverImageUrl", "CreatedAt", "Description", "Name", "PublicId", "Sex", "Status", "Type", "UpdatedAt", "Weight" },
+                values: new object[,]
+                {
+                    { new Guid("4b22475e-dc6d-40be-866f-32f6aae242a9"), 2, "Double doodle", "White", "https://placedog.net/500", new DateTime(2023, 7, 14, 5, 29, 28, 661, DateTimeKind.Utc).AddTicks(1420), "lorem ipsum", "Dee Dee", 1, "Female", 0, "Dog", new DateTime(2023, 7, 14, 5, 29, 28, 661, DateTimeKind.Utc).AddTicks(1420), 10 },
+                    { new Guid("62aa9d4a-40ea-46ab-9186-789618d5e014"), 5, "Bengal cat", "Beige", "https://placekitten.com/200/200", new DateTime(2023, 7, 14, 5, 29, 28, 661, DateTimeKind.Utc).AddTicks(1450), "lorem ipsum", "Buttercup", 2, "Male", 0, "Cat", new DateTime(2023, 7, 14, 5, 29, 28, 661, DateTimeKind.Utc).AddTicks(1450), 5 }
+                });
+        }
+    }
+}
